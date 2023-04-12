db47f7f22657b660abbcb34cf5df166d83731735
boyon99 <bo_yon_99@naver.com>
2023-04-12 15:02:51 +0900

    feat: Redux Middleware 적용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './modules'
+import { store, persistor } from './modules'
 import CounterContainer from './containers/CounterContainer';
 import UserContainer from './containers/UserContainer';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <CounterContainer />
-      <UserContainer />
+      <PersistGate persistor={persistor}>
+        <CounterContainer />
+        <UserContainer />
+      </PersistGate>
     </Provider>
   );
 };
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,13 +1,32 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from "redux";
 
 import counter from "./counter";
 import user from './user'
+import logger from "redux-logger";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
+// reducer
 const rootReducer = combineReducers({
   counter,
   user
 })
 
-const store = createStore(rootReducer)
+// reudx persist
+const persistConfig = {
+  key: "root", // 임의의 key 값
+  storage: storage, // 정확히 어떤 storage 에 저장할지
+  whitelist: ["counter"], // 값을 저장할 리듀서명
+  blacklist: ["user"], // 값을 저장하지 않을 리듀서명
+};
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// store
+export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(logger)));
+// store를 영구보존하는 복사본
+export const persistor = persistStore(store)
+
