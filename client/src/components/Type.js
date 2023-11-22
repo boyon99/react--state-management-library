17842bf97518ba919efae3c52834b287780f5ea8
boyon99 <bo_yon_99@naver.com>
2023-11-22 14:03:59 +0900

    feat: 상품 데이터와 상품 개수 업데이트 함수 가져오기

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './Products';
 import Options from './Options';
 import ErrorBanner from './ErrorBanner';
+import { OrderContext } from '../context/OrderContext';
 
 const Type = ({ orderType }) => {
 
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+
+  // 상품 데이터와 상품 개수 업데이트 함수 가져오기
+  const [orderData, updateItemCount] = useContext(OrderContext);
 
   useEffect(() => {
     loadItems(orderType);
